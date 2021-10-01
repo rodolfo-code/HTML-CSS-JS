abb2e178d7097b4ea9eeb3913ab3da97ae6c1b46
rodolfo_oliveira <rod.vgo@gmail.com>
2021-10-01 11:13:51 -0300

    formulario

--- html/exercicios/form/server.js
+++ html/exercicios/form/server.js
@@ -7,7 +7,13 @@ app.use(bodyParser.urlencoded({extended: true}))
 
 app.post('/usuarios', (req, res) => {
   console.log(req.body)
-  res.send('Parabens')
+  res.send('<h1>Parabens usuario incluido</h1>')
+})
+
+app.post('/usuarios/:id', (req, res) => {
+  console.log(req.params.id)
+  console.log(req.body)
+  res.send('<h1>Parabens usuario alterado</h1>')
 })
 
 app.listen(3000, () => {
